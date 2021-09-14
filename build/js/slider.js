a21acb94bfeacfcc4731c3e5f14f32e8795706d1
AlPro <proningm@gmail.com>
2021-09-14 14:35:28 +0300

    Добавляем в offer предложение про чизкейки

--- build/js/slider.js
+++ build/js/slider.js
@@ -42,6 +42,17 @@ var sliderOfferCream2 = tns({
   nav: false,
   fixedWidth: 200,
 });
+var sliderOfferEskimo = tns({
+  container: '.slider-offer-eskimo',
+  mode: 'carousel',
+  swipeAngle: false,
+  loop: true,
+  gutter: 10,
+  mouseDrag: true,
+  controls: false,
+  nav: false,
+  fixedWidth: 200,
+});
 var feedbackSlider = tns({
   container: '.feedback-slider',
   mode: 'carousel',
