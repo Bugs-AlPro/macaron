76fb69c7ac36c43398fa104a4db518f465570a15
AlPro <proningm@gmail.com>
2021-12-15 14:27:37 +0300

    Добавляем оплату

--- build/js/main.js
+++ build/js/main.js
@@ -4,6 +4,6 @@ const offerItems = document.querySelectorAll('.offer-item__btn');
 for (let offerItem of offerItems) {
   offerItem.addEventListener('click', () => {
     offerItem.classList.toggle('offer-item__btn--open');
-    offerItem.nextElementSibling.classList.toggle('offer-item__list--open');
+    offerItem.nextElementSibling.classList.toggle('offer-item__body--open');
   })
 }
