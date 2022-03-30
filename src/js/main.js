cd363ad64558b064d78cf8944e34831a01f15764
Aleksandr Pronin <proningm@gmail.com>
2022-03-30 23:01:59 +0300

    Создаём основу для добавления на сайт разных форм оплаты

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,10 @@ for (let offerItem of offerItems) {
     offerItem.nextElementSibling.classList.toggle('offer-item__body--open');
   })
 }
+const paymentItems = document.querySelectorAll('.payment__btn');
+for (let paymentItem of paymentItems) {
+  paymentItem.addEventListener('click', () => {
+    paymentItem.classList.toggle('payment__btn--open');
+    paymentItem.nextElementSibling.classList.toggle('payment__description--open');
+  })
+}
