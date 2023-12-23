7fb468eb3cfbd3498cae4644e8cb5af410ea58e0
Aleksandr <proningm@gmail.com>
2023-12-23 18:51:01 +0300

    Добавляем работу гугл аналитики

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -6,7 +6,7 @@ const cookieSection = document.querySelector('.cookie');
 
 function disableAnalytics() {
     var script = document.createElement('script');
-    script.innerHTML = "window['ga-disable-G-GH6SCG1VL8'] = true;"; // Замените UA-XXXXXXXXX-Y на ваш идентификатор отслеживания
+    script.innerHTML = window['ga-disable-G-GH6SCG1VL8'] = true; // Замените UA-XXXXXXXXX-Y на ваш идентификатор отслеживания
     document.head.appendChild(script);
 }
 
