d55d7131813946675e7e59beb6f245f945d75436
Aleksandr <proningm@gmail.com>
2024-01-13 18:39:57 +0300

    Сохраняем временные изменения

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,97 +1,116 @@
-var sliderOfferBase = tns({
-  container: '.sliderOfferBase',
+var mainSlider = tns({
+  container: '.mainSlider',
   mode: 'carousel',
   swipeAngle: false,
   loop: true,
-  gutter: 10,
+  gutter: 15,
   mouseDrag: true,
   controls: false,
   nav: false,
   fixedWidth: 270,
 });
 
-var sliderAllRu = tns({
-  container: '.sliderAllRu',
-  mode: 'carousel',
-  swipeAngle: false,
-  loop: true,
-  gutter: 10,
-  mouseDrag: true,
-  controls: false,
-  nav: false,
-  fixedWidth: 200,
-});
-
-var sliderShellsRu = tns({
-  container: '.sliderOfferShellsRu',
-  mode: 'carousel',
-  swipeAngle: false,
-  loop: true,
-  gutter: 10,
-  mouseDrag: true,
-  controls: false,
-  nav: false,
-  fixedWidth: 200,
-});
+// var courseSlider = tns({
+//   container: '.courseSlider',
+//   mode: 'carousel',
+//   // axis: "vertical",
+//   center: true,
+//   swipeAngle: false,
+//   loop: false,
+//   // gutter: 30,
+//   mouseDrag: false,
+//   controls: true,
+//   nav: false,
+//   items: 1,
+//   fixedWidth: 270,
+//   // animateIn: "slideGo",
+//   // animateOut: "slideOut",
+//   // navPosition: "bottom",
+// });
 
 
-var sliderOfferCreamRu = tns({
-  container: '.sliderOfferCreamRu',
-  mode: 'carousel',
-  swipeAngle: false,
-  loop: true,
-  gutter: 10,
-  mouseDrag: true,
-  controls: false,
-  nav: false,
-  fixedWidth: 200,
-});
+// var sliderAllRu = tns({
+//   container: '.sliderAllRu',
+//   mode: 'carousel',
+//   swipeAngle: false,
+//   loop: true,
+//   gutter: 10,
+//   mouseDrag: true,
+//   controls: false,
+//   nav: false,
+//   fixedWidth: 200,
+// });
 
-var sliderOfferInstRu = tns({
-  container: '.sliderOfferInstRu',
-  mode: 'carousel',
-  swipeAngle: false,
-  loop: true,
-  gutter: 10,
-  mouseDrag: true,
-  controls: false,
-  nav: false,
-  fixedWidth: 200,
-});
+// var sliderShellsRu = tns({
+//   container: '.sliderOfferShellsRu',
+//   mode: 'carousel',
+//   swipeAngle: false,
+//   loop: true,
+//   gutter: 10,
+//   mouseDrag: true,
+//   controls: false,
+//   nav: false,
+//   fixedWidth: 200,
+// });
 
-var sliderOfferFullRu = tns({
-  container: '.sliderOfferFullRu',
-  mode: 'carousel',
-  swipeAngle: false,
-  loop: true,
-  gutter: 10,
-  mouseDrag: true,
-  controls: false,
-  nav: false,
-  fixedWidth: 200,
-});
+
+// var sliderOfferCreamRu = tns({
+//   container: '.sliderOfferCreamRu',
+//   mode: 'carousel',
+//   swipeAngle: false,
+//   loop: true,
+//   gutter: 10,
+//   mouseDrag: true,
+//   controls: false,
+//   nav: false,
+//   fixedWidth: 200,
+// });
+
+// var sliderOfferInstRu = tns({
+//   container: '.sliderOfferInstRu',
+//   mode: 'carousel',
+//   swipeAngle: false,
+//   loop: true,
+//   gutter: 10,
+//   mouseDrag: true,
+//   controls: false,
+//   nav: false,
+//   fixedWidth: 200,
+// });
+
+// var sliderOfferFullRu = tns({
+//   container: '.sliderOfferFullRu',
+//   mode: 'carousel',
+//   swipeAngle: false,
+//   loop: true,
+//   gutter: 10,
+//   mouseDrag: true,
+//   controls: false,
+//   nav: false,
+//   fixedWidth: 200,
+// });
 
 var feedbackSlider = tns({
-  container: '.sliderFeedback',
+  container: '.feedbackSlider',
   mode: 'carousel',
   swipeAngle: false,
-  loop: true,
-  gutter: 10,
+  loop: false,
+  gutter: 20,
   mouseDrag: true,
   controls: false,
   nav: false,
-  fixedWidth: 200,
+  fixedWidth: 350,
 });
 
-var errors = tns({
-  container: '.errors-img-sm',
-  mode: 'carousel',
-  swipeAngle: false,
-  loop: true,
-  gutter: 10,
-  mouseDrag: true,
-  controls: false,
-  nav: false,
-  fixedWidth: 200,
-});
+// var errors = tns({
+//   container: '.errors-img-sm',
+//   mode: 'carousel',
+//   swipeAngle: false,
+//   loop: true,
+//   gutter: 10,
+//   mouseDrag: true,
+//   controls: false,
+//   nav: false,
+//   fixedWidth: 200,
+// });
 
