bb6723b7e4f60852a5c5068080dbfcd16ad89288
Aleksandr <proningm@gmail.com>
2024-02-13 19:27:47 +0300

    Обновление сайта

--- gulpfile.js
+++ gulpfile.js
@@ -64,6 +64,7 @@ export const browserSync = (done) => {
     },
     port: 3000
   });
+
   done();
 };
 
--- src/js/cookie.js
+++ src/js/cookie.js
@@ -4,18 +4,31 @@ const cookieBtnYes = document.querySelector('.cookie__btn--yes');
 const cookieBtnNo = document.querySelector('.cookie__btn--no');
 const cookieSection = document.querySelector('.cookie');
 
-function disableAnalytics() {
+function enableAnalytics() {
     var script = document.createElement('script');
-    script.innerHTML = window['ga-disable-G-GH6SCG1VL8'] = true; // Замените UA-XXXXXXXXX-Y на ваш идентификатор отслеживания
+    // script.innerHTML = window['ga-disable-G-GH6SCG1VL8'] = false; // Замените UA-XXXXXXXXX-Y на ваш идентификатор отслеживания
+
+
+    script.innerHTML =
+        window['ga-disable-G-GH6SCG1VL8'] = false;
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+
+    gtag('js', new Date());
+    gtag('config', 'G-GH6SCG1VL8');
+    // gtag('set', 'allow_google_signals', false);
+
+
+
     document.head.appendChild(script);
 }
 
 cookieBtnNo.addEventListener('click', () => {
     // cookieSection.classList.toggle('cookie--close');
-    disableAnalytics();
+    // disableAnalytics();
 })
 cookieBtnYes.addEventListener('click', () => {
     // cookieSection.classList.toggle('cookie--close');
     fbq('consent', 'grant');
-
+    enableAnalytics();
 })
